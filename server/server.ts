119bf2b7cf1b64e301829b89c4da7bab95e6bd69
Malin2002 <malin.tunheim@gmail.com>
2025-04-22 01:25:02 +0200

    Attempt to get geojson from postgis using hono

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,23 @@
 import { Hono } from "hono";
 import { serve } from "@hono/node-server";
+import pg from "pg";
+
+const postgresql = new pg.Pool({
+  user: "postgres",
+});
 
 const app = new Hono();
 app.get("/", async (c) => {
   return c.text("Hello World");
 });
-app.get("/Forelesning05/api/skoler", (c) => {
+app.get("/Forelesning05/api/skoler", async (c) => {
+  const result = await postgresql.query(
+    `select 
+    skolenavn, 
+    besoksadresse_besoksadresse_adressenavn as adresse,
+    posisjon::json as coordinates
+from grunnskoler_3697913259634315b061b324a3f2cf59.grunnskole`,
+  );
   return c.json({
     type: "FeatureCollection",
     crs: {
@@ -14,7 +26,14 @@ app.get("/Forelesning05/api/skoler", (c) => {
         name: "urn:ogc:def:crs:OGC:1.3:CRS84",
       },
     },
-    features: [],
+    features: result.rows.map(({ coordinates, ...properties }) => ({
+      type: "Feature",
+      properties,
+      geometry: {
+        type: "Point",
+        coordinates,
+      },
+    })),
   });
 });
 serve(app);
