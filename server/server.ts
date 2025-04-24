660441514d8815f0e23a95efd01f0ad75b704954
Malin2002 <malin.tunheim@gmail.com>
2025-04-24 23:13:56 +0200

    Show schools on map through api

--- server/server.ts
+++ server/server.ts
@@ -13,10 +13,12 @@ app.get("/", async (c: Context) => {
 app.get("/Forelesning05/api/skoler", async (c: Context) => {
   const result = await postgresql.query(
     `select 
-    skolenavn, 
-    besoksadresse_besoksadresse_adressenavn as adresse,
-    posisjon::json as coordinates
-from grunnskoler_3697913259634315b061b324a3f2cf59.grunnskole`,
+    skolenavn, fylke.fylkesnummer, st_transform(posisjon, 4326)::json as geometry    
+    from grunnskoler_3697913259634315b061b324a3f2cf59.grunnskole
+        inner join fylker_ba7aea2735714391a98b1a585644e98a.fylke on st_contains(omrade, posisjon)
+    where fylke.objid in (select fylke_fk
+                          from fylker_ba7aea2735714391a98b1a585644e98a.administrativenhetnavn where navn = 'Viken')
+    `,
   );
   return c.json({
     type: "FeatureCollection",
@@ -26,14 +28,16 @@ from grunnskoler_3697913259634315b061b324a3f2cf59.grunnskole`,
         name: "urn:ogc:def:crs:OGC:1.3:CRS84",
       },
     },
-    features: result.rows.map(({ coordinates, ...properties }) => ({
-      type: "Feature",
-      properties,
-      geometry: {
-        type: "Point",
-        coordinates,
-      },
-    })),
+    features: result.rows.map(
+      ({ geometry: { coordinates }, ...properties }) => ({
+        type: "Feature",
+        properties,
+        geometry: {
+          type: "Point",
+          coordinates,
+        },
+      }),
+    ),
   });
 });
 serve(app);
--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,9 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/Forelesning05",
+  server: {
+    proxy: {
+      "/Forelesning05/api": "http://localhost:3000",
+    },
+  },
 });
